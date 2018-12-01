934567df32182b9450f5839d59a3fc1b3e85942c
Rafael Ahrons <rafael.ahrons@gmail.com>
2018-12-01 15:56:04 -0200

    Add Repository and implementation

---
+++ GameRank/Repository/GameResultRepository.cs
@@ -0,0 +1,43 @@
+﻿using GameRank.Context;
+using GameRank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameRank.Repository
+{
+    public class GameResultRepository : IGameResultRepository
+    {
+        private DatabaseContext db = new DatabaseContext();
+
+        public IEnumerable<GameResult> GetTopHundred()
+        {
+            return (from games in db.GameResults
+                    orderby games.Win descending
+                    select new GameResult
+                    {
+                        GameID = games.GameID,
+                        PlayerID = games.PlayerID,
+                        Win = games.Win,
+                        Timestamp = games.Timestamp
+                    }).ToList().Take(100);
+        }
+
+        public bool Add(GameResult gameResult)
+        {
+            if(db.GameResults.Any(e => e.GameID == gameResult.GameID && e.PlayerID == gameResult.PlayerID))
+            {
+                GameResult game = db.GameResults.Single(e => e.GameID == gameResult.GameID && e.PlayerID == gameResult.PlayerID);
+                game.Win += gameResult.Win;
+                game.Timestamp = gameResult.Timestamp;
+                return db.SaveChanges() > 0;
+            }
+            else
+            {
+                db.GameResults.Add(gameResult);
+                return db.SaveChanges() > 0;
+            }
+        }
+    }
+}
---
+++ GameRank/Repository/IGameResultRepository.cs
@@ -0,0 +1,15 @@
+﻿using GameRank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRank.Repository
+{
+    interface IGameResultRepository
+    {
+        IEnumerable<GameResult> GetTopHundred();
+        bool Add(GameResult gameResult);
+    }
+}
