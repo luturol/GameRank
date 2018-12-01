a3c3bbe31d4c4c20d1a0001125f6c7cbf511c825
Rafael Ahrons <rafael.ahrons@gmail.com>
2018-12-01 12:38:41 -0200

    Add initial configurations fot ef and code first

---
+++ GameRank/Controllers/GameRankController.cs
@@ -0,0 +1,33 @@
+﻿using GameRank.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GameRank.Controllers
+{
+    public class GameRankController : ApiController
+    {
+        //Creating Instance of DatabaseContext class  
+        private DatabaseContext db = new DatabaseContext();
+
+        //Creating a method to return Json data   
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                //Prepare data to be returned using Linq as follows  
+                var result = db.GameResults.Select(e => e);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                //If any exception occurs Internal Server Error i.e. Status Code 500 will be returned  
+                return InternalServerError();
+            }
+        }
+    }
+}
--- GameRank/Global.asax.cs
+++ GameRank/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameRank.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,7 @@ namespace GameRank
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            System.Data.Entity.Database.SetInitializer(new DatabaseInitializer());
         }
     }
 }
