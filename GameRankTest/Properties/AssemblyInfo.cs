89fe29d0f16a7e6d282a40404c936c3b816c26c8
Rafael Ahrons <rafael.ahrons@gmail.com>
2018-12-03 22:04:45 -0200

    Add Test for Repository

---
+++ GameRankTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameRankTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameRankTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c8b44b0-1c98-4030-a89f-22d3f1c6b897")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GameRankTest/Repository/TestGameResultRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GameRank.Models;
+using GameRankTest.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameRank.Repository
+{
+    [TestClass]
+    public class TestGameResultRepository
+    {
+        [TestMethod]
+        public void TestSuccessfullyAddNewGameResult()
+        {
+            FakeGameResultRepository fakeGameResultRepository = new FakeGameResultRepository();
+            GameResultDTO FakeGame = new GameResultDTO()
+            {
+                GameID = 1,
+                PlayerID = 1,
+                Timestamp = DateTime.Now,
+                Win = 2
+            };
+            Assert.IsTrue(fakeGameResultRepository.Add(FakeGame));
+        }        
+    }
+}
